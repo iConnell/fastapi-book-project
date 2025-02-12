fd1f843b56be811ca8177be7238c3747ecccad64
Ikechukwu Ugwuanyi <ik.ugwuanyi@gmail.com>
2025-02-12 17:37:05 +0100

    fix: fix double update endpoints

--- api/routes/books.py
+++ api/routes/books.py
@@ -44,12 +44,17 @@ async def get_books() -> OrderedDict[int, Book]:
     return db.get_books()
 
 
-@router.put("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
-async def update_book(book_id: int, book: Book) -> Book:
-    return JSONResponse(
-        status_code=status.HTTP_200_OK,
-        content=db.update_book(book_id, book).model_dump(),
-    )
+@router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
+async def get_book(book_id: int) -> Book:
+    book = db.get_book(book_id)
+
+    if not book:
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={"message": "Book not found."},
+        )
+
+    return book
 
 
 @router.put("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
